6606a43d71c8152a5eda7be6496f9d87de82fdc4
KyleChapman <kyle.chapman@durhamcollege.ca>
2024-05-08 11:07:06 -0400

    First commit - only variable declarations.

--- src/DuckPond.java
+++ src/DuckPond.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DuckPond {
+/*
+ * @author Kyle Chapman
+ * Created: May 8, 2024
+ * Description:
+ * Determine the approximate number of rubber ducks
+ * that can fit on an approximately rectangular pond.
+ */
+
+
+public class DuckPond {
+    public static void main(String[] args) {
+
+        // Declarations.
+        final double DUCK_LENGTH = 0.073;
+        final double DUCK_WIDTH = 0.057;
+        double pond_length;
+        double pond_width;
+        double duck_area;
+        double pond_area;
+
+        // Input.
+
+        // Processing.
+
+        // Output.
+
+    }
 }
